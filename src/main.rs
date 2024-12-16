d8d2387eedeb9a1f838f3b6d6af95a2473d733d5
Gael Zarco <gaelxarco@icloud.com>
2024-12-16 12:39:01 -0800

    Accounting for height vs width differences

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,107 @@
 use minifb::{Window, WindowOptions};
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+const WIDTH: usize = 1080;
+const HEIGHT: usize = 720;
 
-struct FrameBuf {
-    width: usize,
-    height: usize,
-    color: u32, // RGB Color
-    buf: Vec<u32>,
-}
-
-impl FrameBuf {
-    // Creates a new instance of Frame Buffer
-    pub fn new(width: usize, height: usize, color: u32) -> Self {
-        Self {
-            width,
-            height,
-            color,
-            buf: vec![color; width * height],
-        }
-    }
-
-    // Creates 32-bit RGB value using rgb input
-    pub fn u8_rgb_color(r: u8, g: u8, b: u8) -> u32 {
-        let (r, g, b) = (r as u32, g as u32, b as u32);
-        (r << 16) | (g << 8) | b
-    }
-}
-
+// struct FrameBuf {
+//     width: usize,
+//     height: usize,
+//     color: u32, // RGB Color
+//     buf: Vec<u32>,
+// }
 //
-// fn bresenham_line(x0: f32, y0: f32, x1: f32, y1: f32, canvas_buf: Vec<u32>, line: FrameBuf) {
-//     println!("Line: x0: {}, y0: {}, x1: {}, x2: {}", x0, y0, x1, y1);
-//
-//     let mut t = 0.0;
-//
-//     while t < 1.0 {
-//         let x = x0 + (x1 - x0) * t;
-//         let y = y0 + (y1 - y0) * t;
-//         t = t + 0.01;
+// impl FrameBuf {
+//     // Creates a new instance of Frame Buffer
+//     pub fn new(width: usize, height: usize, color: u32) -> Self {
+//         Self {
+//             width,
+//             height,
+//             color,
+//             buf: vec![color; width * height],
+//         }
 //     }
 //
-//     println!("Loop exit");
+//     // Creates 32-bit RGB value using rgb input
+//     pub fn u8_rgb_color(r: u8, g: u8, b: u8) -> u32 {
+//         let (r, g, b) = (r as u32, g as u32, b as u32);
+//         (r << 16) | (g << 8) | b
+//     }
 // }
 
-fn bresenham_line(
-    x0: f32,
-    y0: f32,
-    x1: f32,
-    y1: f32,
+fn u8_rgb_color(r: u8, g: u8, b: u8) -> u32 {
+    let (r, g, b) = (r as u32, g as u32, b as u32);
+    (r << 16) | (g << 8) | b
+}
+
+/// Bresenham line
+///
+/// Takes in x0, y0, x1, and y1 values respectively.
+/// These values are used draw a line of a specified color on a canvas buffer
+///
+/// The canvas buffer consists of a vector of pixels which length equals the total sum of the width x height
+///
+/// The width and height vars are the width and height of the canvas
+fn line(
+    mut x0: f32,
+    mut y0: f32,
+    mut x1: f32,
+    mut y1: f32,
     canvas_buf: &mut Vec<u32>,
     width: usize,
+    height: usize,
     color: u32,
 ) {
-    let mut t = 0.0;
+    let mut steep = false;
 
-    while t < 1.0 {
-        let x = (x0 + (x1 - x0) * t) as usize;
-        let y = (y0 + (y1 - y0) * t) as usize;
+    if (x0-x1).abs() < (y0-y1).abs() { // If the line in steep, we transpose the image
+        (x0, y0) = (y0, x0);
+        (x1, y1) = (y1, x1);
+        steep = true;
+    }
 
-        if x < width && y < canvas_buf.len() / width {
-            let idx = y * width + x;
+    if x0 > x1 { // Make it left to right
+        (x0, x1) = (x1, x0);
+        (y0, y1) = (y1, y0);
+    }
+
+    let mut x = x0;
+    while x <= x1 {
+        // Interpolation parameter. Determines the relative position along the line between the start point and the end point.
+        let t = (x-x0)/(x1-x0); // Ranges between 0.0 - 1.0
+        let y = y0 + t*(y1-y0); // Linear interpolation applied 
+
+        // Convert coordinates to buffer index based on if line is transposed
+        let (dx, dy) = if steep {
+            (y as usize, x as usize)
+        } else {
+            (x as usize, y as usize)
+        };
+
+        // Ensure coordinates are within bounds and draw point 
+        if dx < width && dy < height {
+            let idx = dy * width + dx;
             canvas_buf[idx] = color;
+        } else {
+            println!("ERROR: Coordinates are not within canvas bounds");
         }
 
-        t += 0.01;
+        x += 1.0;
     }
+    
+    // Original Implementation
+    // let mut t = 0.0;
+
+    // while t < 1.0 {
+    //     let x = (x0 + (x1 - x0) * t) as usize;
+    //     let y = (y0 + (y1 - y0) * t) as usize;
+
+    //     if x < width && y < canvas_buf.len() / width {
+    //         let idx = y * width + x;
+    //         canvas_buf[idx] = color;
+    //     }
+
+    //     t += 0.01;
+    // }
 }
 
 fn main() {
@@ -87,28 +124,30 @@ fn main() {
     window.set_target_fps(120);
     window.topmost(true);
 
-    // let line = FrameBuf::new(100, 1, FrameBuf::u8_rgb_color(0, 124, 210));
     let mut canvas_buf = vec![0; WIDTH * HEIGHT];
 
-    // Draw the line at a specific position in the full buffer
-    // w/o the implementation of a line drawing algorithm
-    // for x in 0..line.width {
-    //     if x < WIDTH {
-    //         let idx = x + (HEIGHT / 2) * WIDTH; // Place the line in the middle vertically
-    //         canvas_buf[idx] = line.color;
-    //     }
-    // }
-
-    bresenham_line(
+    line(
         0.0,
         0.0,
         300.0,
         200.0,
         &mut canvas_buf,
         WIDTH,
-        FrameBuf::u8_rgb_color(0, 124, 210),
+        HEIGHT,
+        u8_rgb_color(0, 124, 210),
     );
 
+    line(
+        200.0,
+        300.0,
+        0.0,
+        0.0,
+        &mut canvas_buf,
+        WIDTH,
+        HEIGHT,
+        u8_rgb_color(125, 124, 210),
+    );
+    
     while window.is_open() {
         window
             .update_with_buffer(&canvas_buf, WIDTH, HEIGHT)
